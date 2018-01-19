4ad505ce0e1d5be0807c18b312e546eaa05144a5
Jose Seie <joseeugenioseie@hotmail.com>
2018-01-19 18:06:00 +0200

    slot, form-template, with FormHelper

--- src/AppMain.vue
+++ src/AppMain.vue
@@ -2,8 +2,19 @@
     <div>
 
         <form-helper>
-            <h1 slot="title">{{ title }}</h1>
-            <p slot="text">Inserindo um paragrafo no slot</p>
+
+            <div slot="form-header">
+                <h3>This is a title os the form</h3>
+                <p>Information about the form</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="name" required/>
+                <input type="password" placeholder="password" required/>
+
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Submit</button>
+            </div>
         </form-helper>
 
         <!--<app-header v-bind:title="title" @changeTitle="changeTitle($event)"></app-header>-->
--- src/components/FormHelper.vue
+++ src/components/FormHelper.vue
@@ -1,10 +1,27 @@
 <template>
     <div>
-        <slot name="title"></slot>
+        <h1>Por favor preenche o formulario</h1>
+        <form>
 
-        <h1>I'm a form helper</h1>
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-fields">
+                <slot name="form-fields"></slot>
+            </div>
+            <div id="form-controls">
+                <slot name="form-controls"></slot>
+            </div>
+            <div id="form-links">
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
 
-        <slot name="text"></slot>
+        </form>
     </div>
 </template>
 <script>
@@ -12,6 +29,34 @@
 
     }
 </script>
-<style>
+<style scoped>
+    h1 {
+        text-align: center;
+    }
+    form {
+
+        width: 100%;
+        max-width: 960px;
+        margin: 0 auto;
+    }
+
+    #useful-links ul {
+        padding: 0;
+    }
+    #useful-links li {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    form > div {
+        padding: 20px;
+        background: #eee;
+        margin: 20px 0;
+    }
+    #form-header {
+        background: #ddd;
+        border: 1px solid #bbb;
+    }
+
+
 
 </style>
